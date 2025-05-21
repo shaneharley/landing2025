69cd06018f319c65bf8e2f2dc54319487dccc9a7
Shane Harley <shaneharley@gmail.com>
2025-05-21 16:16:15 +1000

    Added Favicons

---
+++ app/helpers/metadata.js
@@ -0,0 +1,49 @@
+//this sets my metadata
+export const Meta = {
+  title: 'Shane Harley - Product Designer',
+  description: 'Shane Harley - Product Designer',
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/ico",
+        url: "../favicons/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "../favicons/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "../favicons/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "128x128",
+        url: "../favicons/favicon-128x128.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "../favicons/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "../favicons/android-chrome-512x512.png",
+      },
+    ],
+  },
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+}
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-import "./globals.css";
+//import plugins
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
 
+//import styles
+import "./globals.css";
 
-export const metadata = {
-  title: "Shane Harley - Product Designer",
-};
+export { Meta as metadata } from '@/helpers/metadata'
 
 export default function RootLayout({ children }) {
   return (
