a2fdb4ee1b4c8792e0d49c8c650b0b48763c0204
Shane Harley <shaneharley@gmail.com>
2025-05-21 15:34:07 +1000

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
